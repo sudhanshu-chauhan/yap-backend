ff673e1a8f4160f82e271334dea7d800e406e5e3
sudhanshu-chauhan <mailbox.sud@gmail.com>
2022-01-30 06:56:11 +0530

    fix(backend): modify models.go to fix foreign key relationship of user and authcred

--- app/models.go
+++ app/models.go
@@ -1,19 +1,12 @@
 package app
 
-import (
-	"gorm.io/gorm"
-)
-
 type User struct {
-	gorm.Model
-	Name  string `gorm:"type:varchar(128);" json:"name"`
-	Email string `gorm:"primaryKey;type:varchar(128);" json:"email"`
+	Name     string   `gorm:"type:varchar(128);" json:"name"`
+	Email    string   `gorm:"primaryKey;type:varchar(128);" json:"email"`
+	AuthCred AuthCred `gorm:"foreignKey:PasswordHash" json:"authCred"`
 }
 
 type AuthCred struct {
-	gorm.Model
-	UserEmail    string `gorm:"index" json:"userEmail"`
 	Salt         string `gorm:"type:varchar(256);" json:"salt"`
 	PasswordHash string `gorm:"type:varchar(512);" json:"passwordHash"`
-	User         User   `gorm:"foreignkey:Email;references:UserEmail;"`
 }
